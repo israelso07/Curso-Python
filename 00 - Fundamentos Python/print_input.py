cb6c9ceae2d4168755bc42dd44f6a483bb18e7c8
Rael <israelso137@gmail.com>
2026-03-22 01:56:13 -0300

    Test

---
+++ 00 - Fundamentos Python/print_input.py
@@ -0,0 +1,7 @@
+nome = input("Qual é seu nome? ")
+idade = int(input("Qual é sua idade? "))
+cidade = "Paulista"
+
+print(nome)
+print("Seu nome é, ", nome, " e sua idade é: ", idade)
+print(cidade)
--- print_input.py
+++
@@ -1,2 +0,0 @@
-print = input("Qual é seu nome? ")
-
